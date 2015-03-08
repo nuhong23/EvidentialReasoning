b4a193c40c11743c8a3ab66123fc4bb6941522cf
GenomicApplication <tami.thi.le@gmail.com>
2015-03-07 21:49:19 -0800

    parseDataToQuestionFrame now works

--- parseDataToClass.py
+++ parseDataToClass.py
@@ -1,15 +1,6 @@
-
-#s = 'This is a string of words'
-#s.split()
-
-#str.startswith (str, beg=0, end = len(s)); >> True or False
-
-#list as arrays
-# use list = []
-# del list1[2];
-
+# Author Tami Hong Le
+# 3/7/2015
 class ParseDataToClass:
-
 
     def __init__(self):
         pass
@@ -18,30 +9,62 @@ class ParseDataToClass:
 
         #open file, if path does not exist, print error message
         try:
-            dir_path = input("Enter the path to the Input.txt file: ")
-            file = open(dir_path + '/Input.txt', 'r')
-            print(file.readline())
+            dir_path = input("Enter the path to the Input.txt file:   ")
+            inFile = open(dir_path, 'r')
 
-            #parse the file for keywords of frame and CR
-            for line in file:
-                if line.read(1) =="B":
-                    print (line.read(1), end = '')
+            str = ParseDataToClass()
+
+            #parse the file by keywords: Question = Q, frame = B, and compatibility relations = CR
+            for line in inFile:
+
+                #send strings to methods by keyword
+                if line.startswith('Q'):
+                    str.parseDataToQuestionFrame(line)
+                    print (line, end = '')
+                elif line.startswith('B'):
+                    str.parseDataToFrame(line)
+                    print (line, end = '')
+                elif line.startswith("CR"):
+                    str.parseDataToCR(str)
+                    print (line, end = '')
+                else:
+                    print ("Recheck your Input.txt file for consistency.\n")
+                    print ("Question should use the abbreviation 'Q:' ")
+                    print ("Frames should use the abbreviation 'B#:' where # is the sequential number starting from 1")
+                    print ("Compatibility relations should use the abbreviation 'CR#:' where # is the sequential number starting from 1\n")
+                    print ("The error exist in the line --->     " + line, end = '')
+                    inFile.close()
+                    break
+            inFile.close()
 
         #throw an exception and reopen the openInputFile method again
         except FileNotFoundError:
-            print("The Input.txt file you have entered is not valid. Make sure the text file is named ""Input.txt""")
+            print("The directory path to Input.txt file is not valid.\n")
             reopen = ParseDataToClass()
             reopen.openInputFile()
         else:
-            file.close()
+            inFile.close()
+
+    #creates a dictionary for the question frame with two arrays
+    def parseDataToQuestionFrame(self, str):
+
+        splitter = str.split(':',3)
+        quest = [splitter[1], splitter[2]]
+        answers = [splitter[3:len(splitter)]]
+
+        questionFrame = {splitter[0]:[quest,answers]}
+
+        return questionFrame
 
 
+    def parseDataToFrame(self, str):
 
-    def parseDataToFrame(self):
-        pass
+        print ("Entered FRAME")
 
-    def parseDataToCR(self):
-        pass
+        splitter = str.split(':')
+
+    def parseDataToCR(self,str):
+        print ("Entered CR")
 
 
 '''
