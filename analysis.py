8b5f4985dc09fc5526d9e134b95e8e638d8d8a5c
GenomicApplication <tami.thi.le@gmail.com>
2015-03-11 12:43:32 -0700

    updated analysis discount operation

--- analysis.py
+++ analysis.py
@@ -1,12 +1,26 @@
 import numpy as np
 
+# a = np.array([1,0,0])
+#b = np.array([0,1,0])
+#print np.cross(a,b)
+
 class Analysis:
 
     def __init__(self):
         pass
 
-    def discount(self,alpha,mass):
-        return alpha * mass
+    def discount(self, alpha, mass):
+        try:
+            return alpha * mass
+        except ArithmeticError:
+            print("Method can only multiply numbers and not string values.")
+
 
     def translate(self, frame1,frame2):
+        pass
+
+    def fuse(self):
+        pass
+    
+    def interpret(self):
         pass
