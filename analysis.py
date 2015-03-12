114d20caf1792f7e0fadbd04abf1b5be3a34907b
GenomicApplication <tami.thi.le@gmail.com>
2015-03-11 17:49:09 -0700

    updated analysis fuse operation

--- analysis.py
+++ analysis.py
@@ -12,21 +12,23 @@ class Analysis:
     def discount(self, alpha, mass):
         try:
             return alpha * mass
+
         except ArithmeticError:
             print("Method can only multiply numbers and not string values.")
-
+            print("Alpha and mass must be a numerical value")
+            self.discount()
 
     def translate(self, parentFrame, childFrame, frameInfo, propositions):
         pass
 
 
     # Dempster's combination rule
-    def fuse(self, massA, massB):
+    def fuse(self,str, massA, massB):
 
-        def dependent():
+        if str.startswith("dependent"):
             return massA * massB
 
-        def independent():
+        if str.startswith("independent"):
             k = massA * massB
             return 1/(1-k)
 
