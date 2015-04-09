f0f9eb119a280d13015d98c8336c1442979c6387
GenomicApplication <tami.thi.le@gmail.com>
2015-04-09 13:02:46 -0700

    reformatted

--- analysis.py
+++ analysis.py
@@ -34,35 +34,29 @@ class Analysis:
         print(frame2)
 
         value = []
-        x = 0
 
-        try:
-            for i in relations1:
-                for j in relations2:
-                    key =  frame1[4] + ' v ' + str(j)
-                    value = float(frame1[3])
-                self.translatedFrame1[key] = value
-
-            theta = 1 - float(frame1[3])
-            key = "theta"
-            self.translatedFrame1[key] = theta
-
+        for i in relations1:
             for j in relations2:
-                for i in relations1:
-                    key = frame2[4] + ' v ' + str(i)
-                    value = float(frame2[3])
-                self.translatedFrame2[key] = value
+                key =  frame1[4] + ' v ' + str(j)
+                value = float(frame1[3])
+            self.translatedFrame1[key] = value
 
-            theta = 1 - float(frame2[3])
-            key = "theta"
-            self.translatedFrame2[key] = theta
+        theta = 1 - float(frame1[3])
+        key = "theta"
+        self.translatedFrame1[key] = theta
 
-            print(self.translatedFrame1)
-            print(self.translatedFrame2)
+        for j in relations2:
+            for i in relations1:
+                key = frame2[4] + ' v ' + str(i)
+                value = float(frame2[3])
+            self.translatedFrame2[key] = value
 
-        except AttributeError:
-            print("Attribute error in translate operation")
+        theta = 1 - float(frame2[3])
+        key = "theta"
+        self.translatedFrame2[key] = theta
 
+        print(self.translatedFrame1)
+        print(self.translatedFrame2)
 
         self.insertFrame.append(key)
         return self.translatedFrame, self.insertFrame, self.newRelations
