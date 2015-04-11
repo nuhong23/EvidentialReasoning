24e55e56a637e6b8527922047dec11503af370ae
GenomicApplication <tami.thi.le@gmail.com>
2015-04-10 20:36:10 -0700

    reformatted

--- frame.py
+++ frame.py
@@ -28,6 +28,34 @@ class Frames:
         splitter1 = FOD1.split(':')
         splitter2 = FOD2.split(':')
 
+        alpha1 = splitter1[3]
+        alpha2 = splitter2[3].strip()
+        mass1 = splitter1[4].strip()
+        mass2 = splitter2[4].strip()
+
+        discountOption1 = splitter1[2].upper().strip()
+        discountOption2 = splitter2[2].upper().strip()
+
+        #iterates through the FOD list and continues to organize it in preparation for
+        #frame translating
+        for elements in FOD1,FOD2:
+            #accounts for the discount operation before crossing the frames
+            if discountOption1 == 'YES':
+                discounts1 = Analysis()
+                discounts1.discount(alpha1, mass1)
+                splitter1[4] = discounts1.discounted
+                splitter1[3] = 0
+                splitter1[2] = "NO"
+                print(discounts1.discounted)
+
+            elif discountOption2 == 'YES':
+                discounts2 = Analysis()
+                discounts2.discount(alpha2, mass2)
+                splitter2[4] = discounts2.discounted
+                splitter2[3] = 0
+                splitter2[2] = "NO"
+                print(discounts2.discounted)
+
         frameInfo1 = splitter1[1:7]
         relations1.append(splitter1[7].split(','))
         relation1 = splitter1[7].split(',')
@@ -37,32 +65,6 @@ class Frames:
         relations2.append(splitter2[7].split(','))
         relation2 = splitter2[7].split(',')
         print(relations2)
-
-                #iterates through the FOD list and continues to organize it in preparation for
-        #frame translating
-        for elements in FOD1,FOD2:
-            alpha1 = splitter1[3]
-            alpha2 = splitter2[3].strip()
-            mass1 = splitter1[4].strip()
-            mass2 = splitter2[4].strip()
-
-            discountOption1 = splitter1[2].upper().strip()
-            discountOption2 = splitter2[2].upper().strip()
-
-            #accounts for the discount operation before crossing the frames
-            if discountOption1 == 'YES':
-                discounts1 = Analysis()
-                discounts1.discount(alpha1, mass1)
-                splitter1[3] = discounts1.discounted
-                splitter1[2] = "NO"
-                print(discounts1.discounted)
-
-            elif discountOption2 == 'YES':
-                discounts2 = Analysis()
-                discounts2.discount(alpha2, mass2)
-                splitter2[3] = discounts2.discounted
-                splitter2[2] = "NO"
-                print(discounts2.discounted)
 
         cross.translate(frameInfo1, relations1, frameInfo2, relations2)
 
