cc19fadf28051bf90ed478a7e1a98770368c6544
GenomicApplication <tami.thi.le@gmail.com>
2015-04-10 19:59:17 -0700

    reformatted

--- frame.py
+++ frame.py
@@ -38,6 +38,32 @@ class Frames:
         relation2 = splitter2[7].split(',')
         print(relations2)
 
+                #iterates through the FOD list and continues to organize it in preparation for
+        #frame translating
+        for elements in FOD1,FOD2:
+            alpha = float(splitter1[3].strip())
+            alpha = float(splitter2[3].strip())
+            mass = float(splitter1[4].strip())
+            mass = float(splitter2[4].strip())
+
+            discountOption1 = splitter1[2].upper().strip()
+            discountOption2 = splitter2[2].upper().strip()
+
+            #accounts for the discount operation before crossing the frames
+            if discountOption1 == 'YES':
+                print("Entering discount")
+                discounts = Analysis()
+                discounts.discount(alpha, mass)
+                splitter1[3] = discounts.discounted
+                print(discounts.discounted)
+
+            if discountOption2 == 'YES':
+                print("Entering discount")
+                discounts = Analysis()
+                discounts.discount(alpha, mass)
+                splitter2[3] = discounts.discounted
+                print(discounts.discounted)
+
         cross.translate(frameInfo1, relations1, frameInfo2, relations2)
 
         #for i in relation1:
--- main.py
+++ main.py
@@ -26,14 +26,17 @@ def main():
     newFrame = Analysis()
 
     while countFOD != 0:
-        frames.get_crossProductFrames(parse.FOD[0], parse.FOD[1])
-        parse.FOD.remove(parse.FOD[0])
-        parse.FOD.remove(parse.FOD[0])
-        #parse.FOD.insert(0, newFrame.insertFrame)
+        try:
+            frames.get_crossProductFrames(parse.FOD[0], parse.FOD[1])
+            parse.FOD.remove(parse.FOD[0])
+            parse.FOD.remove(parse.FOD[0])
+            #parse.FOD.insert(0, newFrame.insertFrame)
 
-        print("printing parse FOD from main")
-        print(parse.FOD)
-        print(countFOD)
+            print("printing parse FOD from main")
+            print(parse.FOD)
+            print(countFOD)
+        except IndexError:
+            break
 
     print('\n\n')
 
