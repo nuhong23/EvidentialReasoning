7805f3ff8760c3fb764fdddbfcec6f5fe255976c
GenomicApplication <tami.thi.le@gmail.com>
2015-04-10 20:06:57 -0700

    reformatted

--- frame.py
+++ frame.py
@@ -41,26 +41,26 @@ class Frames:
                 #iterates through the FOD list and continues to organize it in preparation for
         #frame translating
         for elements in FOD1,FOD2:
-            alpha = float(splitter1[3].strip())
-            alpha = float(splitter2[3].strip())
-            mass = float(splitter1[4].strip())
-            mass = float(splitter2[4].strip())
+            alpha1 = float(splitter1[3].strip())
+            alpha2 = float(splitter2[3].strip())
+            mass1 = float(splitter1[4].strip())
+            mass2 = float(splitter2[4].strip())
 
             discountOption1 = splitter1[2].upper().strip()
             discountOption2 = splitter2[2].upper().strip()
 
             #accounts for the discount operation before crossing the frames
+            discounts = Analysis()
+
             if discountOption1 == 'YES':
                 print("Entering discount")
-                discounts = Analysis()
-                discounts.discount(alpha, mass)
+                discounts.discount(alpha1, mass1)
                 splitter1[3] = discounts.discounted
                 print(discounts.discounted)
 
             if discountOption2 == 'YES':
                 print("Entering discount")
-                discounts = Analysis()
-                discounts.discount(alpha, mass)
+                discounts.discount(alpha2, mass2)
                 splitter2[3] = discounts.discounted
                 print(discounts.discounted)
 
